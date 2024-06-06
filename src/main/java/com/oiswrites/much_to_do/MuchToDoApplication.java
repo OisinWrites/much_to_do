4240d5350027a1b6f09eabea1f35eb6e15d944dc
OisinWrites <oisin.banville@gmail.com>
2024-06-06 10:40:51 +0100

    Inialised Spring Boot and pom dependancies

---
+++ src/main/java/com/oiswrites/much_to_do/MuchToDoApplication.java
@@ -0,0 +1,13 @@
+package com.oiswrites.much_to_do;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuchToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuchToDoApplication.class, args);
+	}
+
+}
